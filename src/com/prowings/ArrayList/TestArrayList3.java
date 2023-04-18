8eac00c046d9e6efd09ea432d7e3626447cc3f2e
karan <karanbhore64@gmail.com>
2023-04-18 23:06:48 +0530

    hgfhg

--- src/com/prowings/ArrayList/TestArrayList3.java
+++ src/com/prowings/ArrayList/TestArrayList3.java
@@ -5,28 +5,26 @@ import java.util.Iterator;
 
 public class TestArrayList3 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ArrayList al = new ArrayList();
+        ArrayList al = new ArrayList();
 
-		al.add("hello");
-		al.add(20);
-		al.add("hi");
-		al.add(40);
-		al.add("hi");
-		al.add("hello");
+        al.add("hello");
+        al.add(20);
+        al.add("hi");
+        al.add(40);
+        al.add("hi");
+        al.add("hello");
 
-		Iterator itr = al.iterator();
-		System.out.println(itr.next());
+        Iterator itr = al.iterator();
+        System.out.println(itr.next());
 
-//		while (itr.hasNext()) {
-//			System.out.println(itr.next());
-//			if (itr.next().equals(10))
-//				al.set(0, "hellloooo");
-//
-//		}
-//			System.out.println(al);
-//
-		}
-	}
-
+        while (itr.hasNext()) {
+            Object element = itr.next();
+            System.out.println(element);
+            if (element.equals(20))
+                al.set(2, "hellloooo");
+        }
+        System.out.println(al);
+    }
+}
